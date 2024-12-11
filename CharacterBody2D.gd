f50cf109b1f7e7a2490170dd8d52e4c37352e580
BRDstudio <brdstudio23@gmail.com>
2024-12-11 19:03:23 +0100

    init

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -16,6 +16,7 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AudioStreamPlayer.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
