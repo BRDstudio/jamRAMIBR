79b7d31428cb4b7d06c1333cc86bd92f347f93db
BRDstudio <brdstudio23@gmail.com>
2024-12-12 17:41:41 +0100

    init

---
+++ Label2.gd
@@ -0,0 +1,37 @@
+extends Label
+
+# Texto completo que se va a mostrar
+var full_text = ""
+# Velocidad de aparición de las letras (en segundos)
+var letter_delay = 0.06
+# Índice actual de la letra que se muestra
+var current_index = 0
+# Temporizador interno
+var timer = 0.0
+
+func _ready():
+	# Opcional: comienza automáticamente
+	set_full_text("The Main Lines")
+	show_letter_by_letter()
+
+func _process(delta):
+	if current_index < full_text.length():
+		timer += delta
+		if timer >= letter_delay:
+			timer = 0.0
+			current_index += 1
+			text = full_text.substr(0, current_index)
+	else:
+		# Detiene el proceso una vez que termina
+		set_process(false)
+
+func set_full_text(new_text):
+	# Asigna el texto completo y reinicia variables
+	full_text = new_text
+	current_index = 0
+	timer = 0.0
+	text = ""  # Borra el texto actual
+
+func show_letter_by_letter():
+	# Habilita el proceso para mostrar el texto letra por letra
+	set_process(true)
---
+++ vidmen.gd
@@ -0,0 +1,38 @@
+extends VideoStreamPlayer
+
+func _ready():
+	$"../AnimationPlayer".play("new_animation")
+func _on_finished():
+	$".".play()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "new_animation":
+		$"../AnimationPlayer".play("new_animation")
+
+
+func _on_button_mouse_entered():
+	$"../ColorRect2/AudioStreamPlayer".play()
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed():
+	get_tree().quit()
+
+
+
+
+
+func _on_button_2_mouse_entered():
+	$"../ColorRect2/AudioStreamPlayer".play()
+
+
+func _on_button_3_mouse_entered():
+	$"../ColorRect2/AudioStreamPlayer".play()
